9d320d526a1635575c1cf8b894465a1dc26d8673
Sanzhar Karaulov <65087815+unsaiddream@users.noreply.github.com>
2026-09-20 22:26:55 +0500

    blank of QuickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,2 +1,28 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class QuickSort {
+    public static void sort(int[] arr, Metrics metrics){
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        quickSort(arr, 0, arr.length - 1, metrics, 1);
+        }
+    }
+
+    private static void quickSort(
+            int[] arr,
+            int left,
+            int right,
+            Metrics metrics,
+            int depth
+    ) {
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
